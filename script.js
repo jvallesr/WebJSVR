a8863c887a57ccc29cb6ee2f7e7f3c8b106634c9
jvallesr <Jvallesr95@hotmail.com>
2025-11-18 22:53:08 +0400

    prueba 1 git & githup
    html, css y js aderidos de previas pruebas

---
+++ script.js
@@ -0,0 +1,8 @@
+function suma(a, b){    
+  return a+b;
+}
+let numero1 = 5;
+let numero2 = 3;
+let numero3 = 6;
+let resultado=suma(numero1,numero2);
+console.log("el resultado de sumar "+numero1+" y "+numero2+" es igual a "+resultado)
